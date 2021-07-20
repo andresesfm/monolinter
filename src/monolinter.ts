63a57d98124c3a4e12b9bd9e281d5ed05d0b191a
andres.aguilar <andres.aguilar@nfl.com>
2021-07-19 17:16:15 -0700

    using globby instead of glob

--- src/monolinter.ts
+++ src/monolinter.ts
@@ -1,49 +1,47 @@
-import fs from "fs";
-import glob from "glob";
-import {RelatedPackagesValidator} from "./RelatedPackagesValidator";
-import {ConsistentPackageVersionValidator} from "./ConsistentPackageVersionValidator";
-import {ExactPackageVersionValidator} from "./ExactPackageVersionValidator";
-import {DuplicatePackageValidator} from "./DuplicatePackageValidator";
+import fs from 'fs'
+import globby from 'globby'
+import { RelatedPackagesValidator } from './RelatedPackagesValidator'
+import { ConsistentPackageVersionValidator } from './ConsistentPackageVersionValidator'
+import { ExactPackageVersionValidator } from './ExactPackageVersionValidator'
+import { DuplicatePackageValidator } from './DuplicatePackageValidator'
 
 const validators = [
-    new ConsistentPackageVersionValidator(),
-    new DuplicatePackageValidator(),
-    new ExactPackageVersionValidator(),
-    new RelatedPackagesValidator(),
-];
+  new ConsistentPackageVersionValidator(),
+  new DuplicatePackageValidator(),
+  new ExactPackageVersionValidator(),
+  new RelatedPackagesValidator(),
+]
 
 /**
  Finds and reports inconsistencies between packages,
  */
-export default function lint() {
-    console.log("Validating package.json filenames in monorepo");
-    glob("packages/{apps/*,*}/package.json", {}, function (er, filenames) {
-        filenames.push("package.json");
-        filenames.forEach((filename) => {
-            const rawdata = fs.readFileSync(filename);
-            const packageJson = JSON.parse(rawdata.toString());
-            console.log(`Processing ${packageJson.name}: ${filename} ...`);
-            const deps = {
-                ...packageJson.dependencies,
-                ...packageJson.devDependencies,
-            };
-            Object.keys(deps).forEach((dep) => {
-                const version = deps[dep];
-                validators.forEach((validator) => {
-                    validator.addDependency(dep, version, filename);
-                });
-            });
-        });
+export default async function lint(globs: string[]): Promise<number> {
+  console.log('Validating package.json filenames in monorepo')
+  const filenames = await globby(globs)
+  filenames.forEach((filename) => {
+    const rawdata = fs.readFileSync(filename)
+    const packageJson = JSON.parse(rawdata.toString())
+    console.log(`Processing ${packageJson.name}: ${filename} ...`)
+    const deps = {
+      ...packageJson.dependencies,
+      ...packageJson.devDependencies,
+    }
+    Object.keys(deps).forEach((dep) => {
+      const version = deps[dep]
+      validators.forEach((validator) => {
+        validator.addDependency(dep, version, filename)
+      })
+    })
+  })
 
-        const valid = validators.every((validator) => validator.validate());
-        if (valid) {
-            console.log("No validation errors found in dependencies");
-            return 0;
-        } else if (!process.argv.includes("--no-verify")) {
-            console.error(
-                "Please fix before continuing, use flag --no-verify to ignore"
-            );
-            return 1;
-        }
-    });
+  const valid = validators.every((validator) => validator.validate())
+  if (valid) {
+    console.log('No validation errors found in dependencies')
+    return 0
+  } else if (!process.argv.includes('--no-verify')) {
+    console.error(
+      'Please fix before continuing, use flag --no-verify to ignore',
+    )
+    return 1
+  }
 }
